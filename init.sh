2b06b35738a8175f38b6ea053b851eb091603983
Isaias Piña <iax7@users.noreply.github.com>
2025-05-21 22:36:06 -0600

    add --delete option to init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-stow -v -t ~/.config oh-my-posh
-stow -v bash
-stow -v git
-stow -v tmux
-stow -v vim
-stow -v zsh
+PACKAGES=(
+  bash
+  git
+  tmux
+  vim
+  zsh
+)
+
+# stow -v -t ~/.config oh-my-posh
+
+if [[ "$1" == "--delete" || "$1" == "-D" ]]; then
+  options='-v -D'
+  echo "🗑️ Deleting symlinks..."
+else
+  options='-v'
+  echo "🔗 Creating symlinks..."
+fi
+
+for pkg in "${PACKAGES[@]}"; do
+  stow "$options" "$pkg"
+done
