e6cb764a470bfd7be54a03ea56a802a83f56a01f
Isaias Piña <iax7@users.noreply.github.com>
2026-03-03 21:26:44 -0600

    add mise to small

--- init-small.sh
+++ init-small.sh
@@ -3,10 +3,14 @@
 if command -v apt >/dev/null 2>&1; then
   echo "Installing packages for small-rc"
   sudo apt-get update
-  apps=(git zsh vim tmux jq eza bat fzf fd-find ripgrep git-delta \
-        btop zoxide)
+  # Outdated packages: bat eza fd-find fzf ripgrep git-delta zoxide
+  apps=(git zsh vim tmux jq btop)
   sudo apt install -y ${apps[@]}
 fi
+
+# install Mise
+curl https://mise.run | sh
+mise use -g bat delta eza fd fzf ripgrep yazi zellij zoxide
 
 echo "🔗 Creating symlinks..."
 stow -v small-rc
